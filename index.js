a2ee2c391fae6cfae0151b2a48f10f8ad7318c7f
Duncan Kibet <kibet2704@gmail.com>
2025-04-01 00:27:20 -0700

    update changes made

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Peaky) {
+  cats.push(Peaky);
+  
+}
+function destructivelyPrependCat(Fluffy) {
+    cats.unshift(Fluffy);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+function appendCat (Fluffy) {
+    return [...cats, Fluffy];
+}
+function prependCat (Peaky) {
+    return [Peaky, ...cats];
+}
+function removeLastCat (){
+    return cats.slice(0, -1);
+}
+function removeFirstCat () {
+    return cats.slice(1);
+}
